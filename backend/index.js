eb5cc554de26aaaace785fa9144ae40548a1e54c
Rakshith <poojaryrakshith986@gmail.com>
2026-01-31 17:03:15 +0530

    Mmade changes in jenkins file

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,10 @@ const router = require('./routes')
 console.log("url",process.env.FRONTEND_URL);
 
 const app = express()
-// app.use(cors({
-//     origin : process.env.FRONTEND_URL,
-//     credentials:true
-// }))
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials:true
+}))
 app.use(express.json())
 app.use(cookieParser())
 
