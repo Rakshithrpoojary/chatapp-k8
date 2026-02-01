7c5fa103f6c4602d2a0e4f8061f528056644113a
Rakshith <poojaryrakshith986@gmail.com>
2026-02-01 14:21:49 +0530

    Mmade changes in jenkins file

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,11 @@
 const express = require('express')
-const cors = require('cors')
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes')
-console.log("url",process.env.FRONTEND_URL);
 console.log("TOKEN_SECRET_KEY",process.env.TOKEN_SECRET_KEY);
 
-
-
 const app = express()
-app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials:true
-}))
 app.use(express.json())
 app.use(cookieParser())
 
